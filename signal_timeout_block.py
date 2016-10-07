8255a9335646730e54e09bc86f6c12b1338d48c6
Chris Brackert <cbrackert@gmail.com>
2016-10-06 19:21:31 -0600

    Fix strange race condition with persistence
    On rare occassions, a race condition can occur where a
    new timeout is scheduled by a processed signal before
    persisted timeout jobs are scheduled.

--- signal_timeout_block.py
+++ signal_timeout_block.py
@@ -44,21 +44,29 @@ class SignalTimeout(Persistence, GroupBy, Block):
         self._jobs = defaultdict(dict)
         self._jobs_locks = defaultdict(Lock)
         self._repeatable_jobs = defaultdict(dict)
+        self._persisted_jobs = None
 
     def persisted_values(self):
         """Use persistence mixin"""
         return ["_repeatable_jobs"]
 
+    def configure(self, context):
+        super().configure(context)
+        # Save off the persisted jobs here in case self._reapeatable_jobs is
+        # modifed by a processed signal before `start` schedules this.
+        self._persisted_jobs = self._repeatable_jobs
+
     def start(self):
         super().start()
         # Schedule persisted jobs
-        for key in self._repeatable_jobs:
+        for key, intervals in self._persisted_jobs.items():
             with self._jobs_locks[key]:
-                for interval in self._repeatable_jobs[key]:
-                    self._schedule_timeout_job(
-                        self._repeatable_jobs[key][interval],
-                        key, interval, True)
-
+                for interval, job in intervals.items():
+                    if interval not in self._jobs[key]:
+                        # On rare occassions, a new timeout job may have been
+                        # scheduled before this persisted job is scheduled, so
+                        # only do this if one has not been scheduled already.
+                        self._schedule_timeout_job(job, key, interval, True)
 
     def process_signals(self, signals):
         self.for_each_group(self.process_group, signals)
